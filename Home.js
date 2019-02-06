461548b276eca9e841b9ede756139049cd0e8812
sonicheo <43714384+sonicheo@users.noreply.github.com>
2019-02-05 16:28:53 -0800

    Fixed Navigation

--- Home.js
+++ Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export class Home extends Component {
   render () {
     return (
       <View>
         <Text>This is the Home screen</Text>
+        <Button onPress = {() => this.props.navigation.navigate('Settings')}
+        title="Settings"/>
       </View>
     )
   }
